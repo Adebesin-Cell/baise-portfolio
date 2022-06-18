eacb3822544e110afd5361da177315241ceec5d1
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-06-18 13:08:56 +0100

    updating UI

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,32 @@
 import styles from "./Contact.module.scss";
 
 const Contact = function () {
-  return <section className={styles.contact}>&nbsp;</section>;
+  return (
+    <section className={styles.contact} id='contact'>
+      <div className={styles.contact__wrapper}>
+        <div className={styles.contact__left}>
+          <h3 className={styles.contact__title}>
+            <span className={styles["contact__title--green"]}>02.</span>
+            <span className={styles["contact__title--green"]}>
+              What's next?
+            </span>
+          </h3>
+        </div>
+      </div>
+      <h1 className={styles.contact__heading}>Get in touch</h1>
+      <p className={styles.contact__paragraph}>
+        I'm currently looking for connections. Kindly reach out to me if there
+        are any. And if it is just to say hi, my inbox is always open. I will
+        reply as soon as possible.
+      </p>
+      <div className={styles.contact__box}>
+        <a href='/' className={styles.contact__link}>
+          Say Hello
+        </a>
+      </div>
+      <div className={styles.contact__socials}></div>
+    </section>
+  );
 };
 
 export default Contact;
