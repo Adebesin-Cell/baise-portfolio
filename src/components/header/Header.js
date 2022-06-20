711b98b11bdf866094bf22e19f85163161534bbf
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-06-20 12:22:50 +0100

    updating UI

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,16 @@
 import styles from "./Header.module.scss";
 import Logo from "../logo/Logo";
 import Navigation from "../navigation/Navigation";
+import { ReactComponent as MenuIcon } from "../../assets/svgs/icon-menu.svg";
 
 const Header = function (props) {
   return (
     <header className={styles.header}>
       <Logo />
       <Navigation />
+      <div className={styles.header__menu}>
+        <MenuIcon />
+      </div>
     </header>
   );
 };
